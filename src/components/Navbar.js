1e03929438d830ed140b69838f2cd23ef62d1481
Ahmed <ahmed.awais1987@gmai.com>
2024-05-14 18:40:52 +0500

    navbar changes added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,3 @@
-// import React from 'react'
-// import logo from '../assests/images/logo.jpeg';
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-
-//   return (
-//     <div>
-//       <div className='container-navbar'>
-//         <nav className="navbar">
-//         <div className="navbar-brand">
-//         <img src={logo} alt="Logo" className="navbar-logo" />
-//       </div>
-//       <ul className="navbar-menu">
-//         <li className="navbar-item"><Link to="/" className="navbar-link">Home</Link></li> 
-//         <li className="navbar-item"><Link to="/products" className="navbar-link">Products</Link></li> 
-//         <li className="navbar-item"><a href="#services" className="navbar-link">Services</a></li>
-//         <li className="navbar-item"><a href="#contact" className="navbar-link">Contact</a></li>
-//       </ul>
-//     </nav>
-
-//     </div>
-//     </div>
-//   )
-// }
-
-// export default Navbar
-
 
 import React, { useState } from 'react';
 import logo from '../assests/images/logo.jpeg';
--- src/pages/ProductsDetails.js
+++ src/pages/ProductsDetails.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import ProductsDescription from '../components/ProductsDescription';
 import Filters from '../components/Filters';
+import Footer from "../components/Footer";
 import './ProductsDetails.css';
 
 const ProductsDetails = () => {
@@ -10,6 +11,8 @@ const ProductsDetails = () => {
         <Navbar/>
         <ProductsDescription/>
         <Filters/>
+        <Footer/>
+
 
     </div>
   )
